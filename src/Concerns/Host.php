ca6b5c0ba07c8513dacfec3fb523df9be2019dda
Christian Kaal <christiankaal@gmail.com>
2023-10-25 13:34:49 +0200

    Added ability to retrieve a single host

--- src/Concerns/Host.php
+++ src/Concerns/Host.php
@@ -2,6 +2,8 @@
 
 namespace CrixuAMG\Centreon\Concerns;
 
+use CrixuAMG\Centreon\Centreon;
+
 class Host extends CentreonModel
 {
     public function list()
@@ -9,6 +11,29 @@ class Host extends CentreonModel
         return $this->centreon
             ->getClient()
             ->command('show', 'HOST')
+            ->json('result');
+    }
+
+    public function show(string $hostname)
+    {
+        return $this->centreon
+            ->getClient()
+            ->command('show', 'HOST', $hostname)
+            ->json('result.0');
+    }
+
+    public function create(string $hostname, string $alias, string $ipAddress, array $templates = [])
+    {
+        return $this->centreon
+            ->getClient()
+            ->command('add', 'HOST', sprintf(
+                '%s;%s;%s;%s;%s;',
+                $hostname,
+                $alias,
+                $ipAddress,
+                implode(',', $templates),
+                Centreon::POLLER
+            ))
             ->json('result');
     }
 }
